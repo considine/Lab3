743e1f66d0fd2bbcb26bede97b127e01888625df
consodouche <jackconsidine1234@aol.com>
2015-02-09 23:04:39 -0500

    basdf
    bankacccount.cpp , h and makefile

---
+++ 7cb74407/bankaccount.h
@@ -0,0 +1,16 @@
+// John Considine Harrison Le
+//    Feb 4, 2014
+//            Lab 3
+
+#ifndef 
+#define BANKACCOUNT_H
+using namespace std;
+
+class bankaccount  {
+	public:
+	protected://stuff that only inherited members have access to
+		int accountnum;// bank account number
+		int pin; //the pin number for associated accounts
+};
+
+//this is a test
